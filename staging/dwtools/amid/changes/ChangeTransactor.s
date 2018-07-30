d72eff69d9ba49b5f6179b0f2b1416bc35df7975
Wandalen <wandalen.me@gmail.com>
2018-07-30 19:58:34 +0300

    sync

--- staging/dwtools/amid/changes/ChangeTransactor.s
+++ staging/dwtools/amid/changes/ChangeTransactor.s
@@ -35,7 +35,7 @@ var _ObjectHasOwnProperty = Object.hasOwnProperty;
 
 //
 
-function onMixin( dstClass )
+function onMixin( mixinDescriptor, dstClass )
 {
 
   var dstPrototype = dstClass.prototype;
